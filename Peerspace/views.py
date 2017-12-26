d81e4d6e7eaac6a6a46f5d2c577f2546ec94cfa1
JamieWilliamsHackIT <mail@jamiewilliams.co.uk>
2017-12-26 18:48:04 +0000

    More clean up

--- Peerspace/views.py
+++ Peerspace/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 from users.models import User
 from posts.models import Post
 
+# Use this url for production
+root_url = 'https://peerspace.herokuapp.com'
+# Use this url for development
+# root_url = 'https://127.0.0.8:8000'
+
 def home_view(request):
     users = User.objects.all()
     posts = Post.objects.all()
-    return render(request, 'home.html', {'users': users, 'posts': posts})
+    return render(request, 'home.html',
+                    {
+                        'users': users,
+                        'posts': posts,
+                        'root_url': root_url,
+                    }
+                 )
